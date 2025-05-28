8ad3f1963fe08f5eeff72d2cb1d174f30dcdf07d
MKM <mkmkin@protonmail.com>
2025-05-28 11:52:05 +0100

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-2xl font-semibold"
       >
-        Hi, I'm KinCharge, your AI Assistant!
+        Hi, this is KinCharge, your AI Assistant!
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
