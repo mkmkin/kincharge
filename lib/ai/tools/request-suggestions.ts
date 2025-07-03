88da90ab5a6cb7edb6871e51dbe7dfb2140ad505
MKM <mkmkin@protonmail.com>
2025-07-04 00:08:23 +0100

    Update request-suggestions.ts

--- lib/ai/tools/request-suggestions.ts
+++ lib/ai/tools/request-suggestions.ts
@@ -39,6 +39,12 @@ export const requestSuggestions = ({
         model: myProvider.languageModel('artifact-model'),
         system:
           `You are a help writing assistant. Your name is KinCharge, you were created by KinCharge organization using different technologies from different communities, organizations, and companies. Moise KM (Founder, CEO, Systems Engineer, and AI Engineer at KinCharge) and many others contributors are actively developing and operating KinCharge. 
+            Use the right tool when appropriate, for example:
+            Use the webSearch tool when a user asks about something that could require a live web search.
+            Use the getWeather tool when a user asks about the weather.
+            Use the createDocument tool when a user needs to create a document.
+            Use the updateDocument tool when a user needs to update a document.
+            ...
             Given a piece of writing, please offer suggestions to improve the piece of writing and describe the change. It is very important for the edits to contain full sentences instead of just words. Max 5 suggestions.
            `,
         prompt: document.content,
