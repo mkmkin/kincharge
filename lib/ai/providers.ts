2fd8384c0e124f870eb85c030fafc24eecdcc060
MKM <mkmkin@protonmail.com>
2025-07-04 00:11:55 +0100

    Update providers.ts

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -23,15 +23,15 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-vision-1212'),
+        'chat-model': xai('grok-3-latest'),
         'chat-model-reasoning': wrapLanguageModel({
-          model: xai('grok-3-mini-beta'),
+          model: xai('grok-3-mini-latest'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),     
+        'title-model': xai('grok-3-latest'),
+        'artifact-model': xai('grok-3-latest'),     
       },
       imageModels: {
-        'small-model': xai.image('grok-2-image'),
+        'small-model': xai.image('grok-2-image-latest'),
       },
     });
