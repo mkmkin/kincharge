fc7799305946fb80a8c210c8867c296ec0f42196
Moïse KM <mkmtechnology7@gmail.com>
2025-07-18 09:56:43 -0400

    commit

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -98,8 +98,9 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p >
                   
-                  { `Get a Custom Artificial Intelligence System with all KinCharge AI System Capabilities and Functionalities (Assistance, Automation, Conversation, Text Generation, Text Analysis, Data Analysis, Document Analysis, Document/File Creation and Editing, Image and Video Analysis, Audio Analysis, Audio and Video Transcription, Image and Video Generation, Audio Generation, and more…).
-` }
+                  { `
+                      Order and get a Custom Artificial Intelligence System with all KinCharge AI System Capabilities and Functionalities: Assistance, Automation, Conversation, Text Generation, Text Analysis, Data Analysis, Document Analysis, Document/File Creation and Editing, Image and Video Analysis, Audio Analysis, Audio and Video Transcription, Image and Video Generation, Audio Generation, and more…
+                    ` }
                 </p>
                 <p>
                   <a href={`tel:‪+19298776893‬`} className="underline"> 
@@ -130,13 +131,13 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               <AlertDialogTitle className="text-xl">KinCharge Agency</AlertDialogTitle>
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p>
-                  KinCharge Agency helps you meet your needs and solve your problems with useful services offered by us and by our partners. 
+                  KinCharge Agency helps you meet your needs and solve your problems with useful services provided by us and by our partners.
                   You can{ " " }
                   <a href={`tel:‪+19298776893‬`} className="underline"> 
                   contact us
                   </a>{ " " } about any of the following services.
-                  The main services we provide are: Artificial Intelligence Integration with Other Technological Systems, with Organizational Systems, and with Natural Systems and Natural Intelligence; All Technical/Technology Works and Services; Recruiting and Staffing; Training and Education. We also provide the following extra services: Sales and Purchases; Rentals; Bookings; Visas; Currency Exchange; Money Transfer; Refills/Recharges; Bills payments.
-                </p>
+                  The main services we provide are: 1) Artificial Intelligence Integration with Other Technological Systems, with Organizational Systems, and with Natural Systems and Natural Intelligence; 2) All Technical/Technology Works and Services; 3) Recruiting and Staffing; 4) Training and Education. We also provide the following extra services: 1) Sales and Purchases; 2) Rentals; 3) Bookings; 4) Visas; 5) Currency Exchange; 6) Money Transfer; 7) Refills/Recharges; 8) Bills Payments.
+                  </p>
                 <p>
                   <Link href="https://agency.kincharge.com" target="_blank" className="underline hidden">Go to KinCharge Agency</Link>
                    <br className="hidden"/>
@@ -167,7 +168,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               <AlertDialogTitle className="text-xl">KinCharge Other Systems</AlertDialogTitle>
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p>
-                  { `KinCharge currently only provides KinCharge AI System, which is our main and flashship system. More systems will be added progressively.` }
+                  { `KinCharge currently provides the following systems: AI System, and AI System R. More systems will be added progressively.` }
                   <Link href="https://offerings.kincharge.com" target="_blank" className="underline hidden">our other systems</Link>                
                 </p>
               </AlertDialogDescription>
@@ -192,7 +193,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               <AlertDialogTitle className="text-xl">About KinCharge</AlertDialogTitle>
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p>
-                  { `KinCharge's mission is to supercharge and maximize the world’s efficiency and satisfaction with artificial intelligence integrated with other technological systems, with organizational systems, and with natural systems and natural intelligence. In addition, KinCharge is committed to user/customer satisfaction, safety, security, and privacy, open source, open technologies and systems, technology and system maintenance and operation, innovation, technological progress, research, and education.` }                 
+                  { `KinCharge's mission is to supercharge and maximize the world’s efficiency and satisfaction with artificial intelligence integrated with other technological systems, with organizational systems, and with natural systems and natural intelligence. In addition, KinCharge is committed to user/customer satisfaction, safety, security, and privacy, open source, open technologies and systems, technologies and systems engineering, development, operation, and maintenance, technological progress, research, and education.` }                 
                 <Link href="https://offerings.kincharge.com" target="_blank" className="underline hidden">our other offerings</Link>                
                 </p>
                 <br />
--- components/greeting.tsx
+++ components/greeting.tsx
@@ -105,7 +105,7 @@ export const Greeting = () => {
               <AlertDialogTitle className="text-xl">Custom AI System</AlertDialogTitle>
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p >
-                  Get a Custom Artificial Intelligence System with all KinCharge AI System Capabilities and Functionalities (Assistance, Automation, Conversation, Text Generation, Text Analysis, Data Analysis, Document Analysis, Document/File Creation and Editing, Image and Video Analysis, Audio Analysis, Audio and Video Transcription, Image and Video Generation, Audio Generation, and more…).
+                  Order and get a Custom Artificial Intelligence System with all KinCharge AI System Capabilities and Functionalities: Assistance, Automation, Conversation, Text Generation, Text Analysis, Data Analysis, Document Analysis, Document/File Creation and Editing, Image and Video Analysis, Audio Analysis, Audio and Video Transcription, Image and Video Generation, Audio Generation, and more…
                 </p>
                 <p>
                   <a href={`tel:‪+19298776893‬`} className="underline"> 
@@ -136,7 +136,7 @@ export const Greeting = () => {
               <AlertDialogTitle className="text-xl">KinCharge Other Systems</AlertDialogTitle>
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p>
-                  { `KinCharge currently only provides KinCharge AI System, which is our main and flashship system. More systems will be added progressively.` }
+                  { `KinCharge currently provides the following systems: AI System, and AI System R. More systems will be added progressively.` }
                   <Link href="https://offerings.kincharge.com" target="_blank" className="underline hidden">our other offerings</Link>                
                 </p>
               </AlertDialogDescription>
@@ -161,12 +161,12 @@ export const Greeting = () => {
               <AlertDialogTitle className="text-xl">KinCharge Agency</AlertDialogTitle>
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p>
-                  KinCharge Agency helps you meet your needs and solve your problems with useful services offered by us and by our partners. 
+                  KinCharge Agency helps you meet your needs and solve your problems with useful services provided by us and by our partners. 
                   You can{ " " }
                   <a href={`tel:‪+19298776893‬`} className="underline"> 
                   contact us
                   </a>{ " " } about any of the following services.
-                  The main services we provide are: Artificial Intelligence Integration with Other Technological Systems, with Organizational Systems, and with Natural Systems and Natural Intelligence; All Technical/Technology Works and Services; Recruiting and Staffing; Training and Education. We also provide the following extra services: Sales and Purchases; Rentals; Bookings; Visas; Currency Exchange; Money Transfer; Refills/Recharges; Bills payments.
+                  The main services we provide are: 1) Artificial Intelligence Integration with Other Technological Systems, with Organizational Systems, and with Natural Systems and Natural Intelligence; 2) All Technical/Technology Works and Services; 3) Recruiting and Staffing; 4) Training and Education. We also provide the following extra services: 1) Sales and Purchases; 2) Rentals; 3) Bookings; 4) Visas; 5) Currency Exchange; 6) Money Transfer; 7) Refills/Recharges; 8) Bills Payments.
                 </p>
                 <p>
                   <Link href="https://agency.kincharge.com" target="_blank" className="underline hidden">Go to KinCharge Agency</Link>
