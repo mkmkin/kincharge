113d4e6aebd4eda474e25d09105bcd6cfea2b97e
MKM <mkmkin@protonmail.com>
2025-11-08 12:42:23 -0500

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -153,30 +153,30 @@ KinCharge is committed to user/customer satisfaction, efficiency, wellness, safe
         <AlertDialog>
           <AlertDialogTrigger>
             <span className="underline">
-              Our Community
+              Our Agency
             </span>
           </AlertDialogTrigger>
           <AlertDialogContent>
             <AlertDialogHeader>
-              <AlertDialogTitle className="text-xl">KinCharge Community</AlertDialogTitle>
+              <AlertDialogTitle className="text-xl">KinCharge Agency</AlertDialogTitle>
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p>
-                  KinCharge community locations provide technology products and services to help the community fulfill their needs, in collaboration with our partners. 
+                  KinCharge Agency locations provide technology services, products, and systems to help the community fulfill their needs, in collaboration with our partners. 
                   You can{ " " }
                   <a href={`tel:‪+19298776893‬`} className="underline"> 
                   contact us
-                  </a>{ " " } about any of the following products and services.
-                  The main products and services we provide are: 1) Artificial Intelligence 
+                  </a>{ " " } about any of the following technology services, products, and systems.
+                  The main services, products, and systems we provide are: 1) Artificial Intelligence 
 
-2) Systems Engineering 
+2) Integrated Systems  
 
 3) Information Technology, Informatics, Electronics, and Telecommunications
 
 4) Mechatronics and Mechanics
 
-5) Construction, Prefab, Self-Sufficiency, Electricity, Plumbing, Equipment…
+5) Infrastructure, Construction, Electricity, Plumbing, Equipment...
 
-6) Manufacturing and Customization 
+6) Manufacturing, Customization, Repair, Upgrading...
 
 7) Waste Collection, Treatment, Disposal and Recycling
 
@@ -184,11 +184,11 @@ KinCharge is committed to user/customer satisfaction, efficiency, wellness, safe
 
 8) Agriculture, Mining, and more…
 
- We also provide the following extra products and services: 1) Training and Education
+ We also provide the following complementary services/products: 1) Training, Education, Research
 
 2) Recruiting and Staffing
 
-3) Fitness, Wellness, Efficiency, Satisfaction
+3) Fitness, Wellness, Efficiency, Community, Satisfaction
 
 4) Security, Safety, and Privacy 
 
@@ -233,14 +233,19 @@ KinCharge is committed to user/customer satisfaction, efficiency, wellness, safe
               <AlertDialogTitle className="text-xl">About KinCharge</AlertDialogTitle>
               <AlertDialogDescription className="text-lg max-h-80 overflow-y-scroll">
                 <p>
-                  { `KinCharge's mission is to supercharge and maximize the world’s efficiency, satisfaction, and wellness with eco-friendly, intelligent, community-driven, and open source technology products, services, and systems (artificial intelligence, informatics/information technology, electronics, mechatronics, and other technologies and systems).
+                  { `KinCharge's mission is to supercharge and maximize the world’s efficiency, satisfaction, and wellness with eco-friendly, intelligent, community-driven, and open source technology services, products, and systems (artificial intelligence, informatics/information technology, electronics, mechatronics, and other technologies and systems).
 
 KinCharge is committed to user/customer satisfaction, efficiency, wellness, safety, security, and privacy, open source, open technology and systems, sustainability, technology and systems engineering, development, operation, and maintenance, technological progress, research, and education.` }                 
                 <Link href="https://offerings.kincharge.com" target="_blank" className="underline hidden">our other offerings</Link>                
                 </p>
                 <br />
+
                 <p>
-                  KinCharge AI Assistant (Application, Cloud Server, Local Server) currently has the following Capabilities: Search the web and summarize the web search result for you. Analyze and summarize documents, texts, code, and images for you. Generate, create, and edit documents, texts, and code for you (image, video, and audio generation will be added soon). The first full version of KinCharge with more AI System capabilities and functionalities will be fully released by November 2025 (advanced tasks, advanced assistance, advanced automation, full encryption, and more…).‬</p>
+                  KinCharge Agency provides technology services, products, & systems to fulfill our community needs, 24/7, in collaboration with our partners.
+                </p>
+                <p>
+                  KinCharge AI Assistant (Application, Cloud Server, Local Server) currently has the following Capabilities: Search the web and summarize the web search result for you. Analyze and summarize documents, texts, code, and images for you. Generate, create, and edit documents, texts, and code for you (image, video, and audio generation will be added soon). The first full version of KinCharge with more AI System capabilities and functionalities will be fully released by November 2025 (advanced tasks, advanced assistance, advanced automation, full encryption, and more…).‬
+                </p>
                 <p>
                   <Link href="https://agency.kincharge.com" target="_blank" className="underline hidden">Go to KinCharge Agency</Link>
                   <br className="hidden" />
